ec3d460bb91570035c38a0e00d1670ba0a279a70
Matias Brignone <mnbrignone@gmail.com>
2019-06-30 20:08:52 -0300

    convert spaces to tabs

--- main/wifi.c
+++ main/wifi.c
@@ -60,11 +60,11 @@ static int send_http_request(int socket_handler, struct addrinfo* res, char* htt
 void initialize_wifi()
 {
 	// create a queue capable of containing a single pointer to struct addrinfo
-    queue_wifi_tx_to_rx = xQueueCreate(1, sizeof(struct addrinfo *));
-    if (queue_wifi_tx_to_rx == NULL)
-    {
-    	printf("Could not create wifi_tx_to_rx QUEUE.\n");
-    }
+	queue_wifi_tx_to_rx = xQueueCreate(1, sizeof(struct addrinfo *));
+	if (queue_wifi_tx_to_rx == NULL)
+	{
+		printf("Could not create wifi_tx_to_rx QUEUE.\n");
+	}
 
 	// create the event group to handle wifi events
 	wifi_event_group = xEventGroupCreate();
@@ -165,26 +165,26 @@ void wifi_tx_task(void *pvParameter)
 				bzero(recv_buf, sizeof(recv_buf));
 				r = lwip_read(s, recv_buf, sizeof(recv_buf) - 1);
 
-  				if (strstr (recv_buf,"Status") != NULL && strstr (recv_buf,"200 OK") != NULL)
-  				{
-  					flag_rsp_ok = 1;
-  				}
+				if (strstr (recv_buf,"Status") != NULL && strstr (recv_buf,"200 OK") != NULL)
+				{
+					flag_rsp_ok = 1;
+				}
 
-  				// check if the CONTENT of the response arrived
-  				pch = strstr(recv_buf, "\n\r\n");
-  				if (pch != NULL || flag_content == 1)
-  				{
-  					if (pch != NULL)
-  					{
-  						strcat(content_buf, pch+3);	// pch + 3 to ignore the LF+CR+LF
-  					}
-  					else
-  					{
-  						strcat(content_buf, recv_buf);
-  					}
-  					
-  					flag_content = 1;
-  				}
+				// check if the CONTENT of the response arrived
+				pch = strstr(recv_buf, "\n\r\n");
+				if (pch != NULL || flag_content == 1)
+				{
+					if (pch != NULL)
+					{
+						strcat(content_buf, pch+3);	// pch + 3 to ignore the LF+CR+LF
+					}
+					else
+					{
+						strcat(content_buf, recv_buf);
+					}
+					
+					flag_content = 1;
+				}
 
 			} while(r > 0);
 
@@ -222,215 +222,215 @@ void wifi_tx_task(void *pvParameter)
 void wifi_secure_tx_task(void *pvParameter)
 {
 	char buf[512];
-    int ret, flags, len;
+	int ret, flags, len;
 
-    mbedtls_entropy_context entropy;
-    mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_ssl_context ssl;
-    mbedtls_x509_crt cacert;
-    mbedtls_ssl_config conf;
-    mbedtls_net_context server_fd;
+	mbedtls_entropy_context entropy;
+	mbedtls_ctr_drbg_context ctr_drbg;
+	mbedtls_ssl_context ssl;
+	mbedtls_x509_crt cacert;
+	mbedtls_ssl_config conf;
+	mbedtls_net_context server_fd;
 
-    mbedtls_ssl_init(&ssl);
-    mbedtls_x509_crt_init(&cacert);
-    mbedtls_ctr_drbg_init(&ctr_drbg);
-    ESP_LOGI(TAG, "Seeding the random number generator");
+	mbedtls_ssl_init(&ssl);
+	mbedtls_x509_crt_init(&cacert);
+	mbedtls_ctr_drbg_init(&ctr_drbg);
+	ESP_LOGI(TAG, "Seeding the random number generator");
 
-    mbedtls_ssl_config_init(&conf);
+	mbedtls_ssl_config_init(&conf);
 
-    mbedtls_entropy_init(&entropy);
-    if((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-                                    NULL, 0)) != 0)
-    {
-        ESP_LOGE(TAG, "mbedtls_ctr_drbg_seed returned %d", ret);
-        abort();
-    }
+	mbedtls_entropy_init(&entropy);
+	if((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+									NULL, 0)) != 0)
+	{
+		ESP_LOGE(TAG, "mbedtls_ctr_drbg_seed returned %d", ret);
+		abort();
+	}
 
-    ESP_LOGI(TAG, "Loading the CA root certificate...");
+	ESP_LOGI(TAG, "Loading the CA root certificate...");
 
-    ret = mbedtls_x509_crt_parse(&cacert, server_root_cert_pem_start,
-                                 server_root_cert_pem_end-server_root_cert_pem_start);
+	ret = mbedtls_x509_crt_parse(&cacert, server_root_cert_pem_start,
+								 server_root_cert_pem_end-server_root_cert_pem_start);
 
-    if(ret < 0)
-    {
-        ESP_LOGE(TAG, "mbedtls_x509_crt_parse returned -0x%x\n\n", -ret);
-        abort();
-    }
+	if(ret < 0)
+	{
+		ESP_LOGE(TAG, "mbedtls_x509_crt_parse returned -0x%x\n\n", -ret);
+		abort();
+	}
 
-    ESP_LOGI(TAG, "Setting hostname for TLS session...");
+	ESP_LOGI(TAG, "Setting hostname for TLS session...");
 
-     /* Hostname set here should match CN in server certificate */
-    if((ret = mbedtls_ssl_set_hostname(&ssl, WEB_SERVER)) != 0)
-    {
-        ESP_LOGE(TAG, "mbedtls_ssl_set_hostname returned -0x%x", -ret);
-        abort();
-    }
+	 /* Hostname set here should match CN in server certificate */
+	if((ret = mbedtls_ssl_set_hostname(&ssl, WEB_SERVER)) != 0)
+	{
+		ESP_LOGE(TAG, "mbedtls_ssl_set_hostname returned -0x%x", -ret);
+		abort();
+	}
 
-    ESP_LOGI(TAG, "Setting up the SSL/TLS structure...");
+	ESP_LOGI(TAG, "Setting up the SSL/TLS structure...");
 
-    if((ret = mbedtls_ssl_config_defaults(&conf,
-                                          MBEDTLS_SSL_IS_CLIENT,
-                                          MBEDTLS_SSL_TRANSPORT_STREAM,
-                                          MBEDTLS_SSL_PRESET_DEFAULT)) != 0)
-    {
-        ESP_LOGE(TAG, "mbedtls_ssl_config_defaults returned %d", ret);
-        goto exit;
-    }
+	if((ret = mbedtls_ssl_config_defaults(&conf,
+										  MBEDTLS_SSL_IS_CLIENT,
+										  MBEDTLS_SSL_TRANSPORT_STREAM,
+										  MBEDTLS_SSL_PRESET_DEFAULT)) != 0)
+	{
+		ESP_LOGE(TAG, "mbedtls_ssl_config_defaults returned %d", ret);
+		goto exit;
+	}
 
-    mbedtls_ssl_conf_read_timeout(&conf, 1000);
+	mbedtls_ssl_conf_read_timeout(&conf, 1000);
 
-    /* MBEDTLS_SSL_VERIFY_OPTIONAL is bad for security, in this example it will print
-       a warning if CA verification fails but it will continue to connect.
+	/* MBEDTLS_SSL_VERIFY_OPTIONAL is bad for security, in this example it will print
+	   a warning if CA verification fails but it will continue to connect.
 
-       You should consider using MBEDTLS_SSL_VERIFY_REQUIRED in your own code.
-    */
-    mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_OPTIONAL);
-    mbedtls_ssl_conf_ca_chain(&conf, &cacert, NULL);
-    mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
+	   You should consider using MBEDTLS_SSL_VERIFY_REQUIRED in your own code.
+	*/
+	mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_OPTIONAL);
+	mbedtls_ssl_conf_ca_chain(&conf, &cacert, NULL);
+	mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
 #ifdef CONFIG_MBEDTLS_DEBUG
-    mbedtls_esp_enable_debug_log(&conf, 4);
+	mbedtls_esp_enable_debug_log(&conf, 4);
 #endif
 
-    if ((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0)
-    {
-        ESP_LOGE(TAG, "mbedtls_ssl_setup returned -0x%x\n\n", -ret);
-        goto exit;
-    }
+	if ((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0)
+	{
+		ESP_LOGE(TAG, "mbedtls_ssl_setup returned -0x%x\n\n", -ret);
+		goto exit;
+	}
 
-    while(1) {
-        /* Wait for the callback to set the CONNECTED_BIT in the
-           event group.
-        */
-        xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
-                            false, true, portMAX_DELAY);
-        ESP_LOGI(TAG, "Connected to AP");
+	while(1) {
+		/* Wait for the callback to set the CONNECTED_BIT in the
+		   event group.
+		*/
+		xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+							false, true, portMAX_DELAY);
+		ESP_LOGI(TAG, "Connected to AP");
 
-        mbedtls_net_init(&server_fd);
+		mbedtls_net_init(&server_fd);
 
-        ESP_LOGI(TAG, "Connecting to %s:%s...", WEB_SERVER, WEB_PORT);
+		ESP_LOGI(TAG, "Connecting to %s:%s...", WEB_SERVER, WEB_PORT);
 
-        if ((ret = mbedtls_net_connect(&server_fd, WEB_SERVER,
-                                      WEB_PORT, MBEDTLS_NET_PROTO_TCP)) != 0)
-        {
-            ESP_LOGE(TAG, "mbedtls_net_connect returned -%x", -ret);
-            goto exit;
-        }
+		if ((ret = mbedtls_net_connect(&server_fd, WEB_SERVER,
+									  WEB_PORT, MBEDTLS_NET_PROTO_TCP)) != 0)
+		{
+			ESP_LOGE(TAG, "mbedtls_net_connect returned -%x", -ret);
+			goto exit;
+		}
 
-        ESP_LOGI(TAG, "Connected.");
+		ESP_LOGI(TAG, "Connected.");
 
-        mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, NULL, mbedtls_net_recv_timeout);
+		mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, NULL, mbedtls_net_recv_timeout);
 
-        ESP_LOGI(TAG, "Performing the SSL/TLS handshake...");
+		ESP_LOGI(TAG, "Performing the SSL/TLS handshake...");
 
-        while ((ret = mbedtls_ssl_handshake(&ssl)) != 0)
-        {
-            if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE)
-            {
-                ESP_LOGE(TAG, "mbedtls_ssl_handshake returned -0x%x", -ret);
-                goto exit;
-            }
-        }
+		while ((ret = mbedtls_ssl_handshake(&ssl)) != 0)
+		{
+			if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE)
+			{
+				ESP_LOGE(TAG, "mbedtls_ssl_handshake returned -0x%x", -ret);
+				goto exit;
+			}
+		}
 
-        ESP_LOGI(TAG, "Verifying peer X.509 certificate...");
+		ESP_LOGI(TAG, "Verifying peer X.509 certificate...");
 
-        if ((flags = mbedtls_ssl_get_verify_result(&ssl)) != 0)
-        {
-            /* In real life, we probably want to close connection if ret != 0 */
-            ESP_LOGW(TAG, "Failed to verify peer certificate!");
-            bzero(buf, sizeof(buf));
-            mbedtls_x509_crt_verify_info(buf, sizeof(buf), "  ! ", flags);
-            ESP_LOGW(TAG, "verification info: %s", buf);
-        }
-        else {
-            ESP_LOGI(TAG, "Certificate verified.");
-        }
+		if ((flags = mbedtls_ssl_get_verify_result(&ssl)) != 0)
+		{
+			/* In real life, we probably want to close connection if ret != 0 */
+			ESP_LOGW(TAG, "Failed to verify peer certificate!");
+			bzero(buf, sizeof(buf));
+			mbedtls_x509_crt_verify_info(buf, sizeof(buf), "  ! ", flags);
+			ESP_LOGW(TAG, "verification info: %s", buf);
+		}
+		else {
+			ESP_LOGI(TAG, "Certificate verified.");
+		}
 
-        ESP_LOGI(TAG, "Cipher suite is %s", mbedtls_ssl_get_ciphersuite(&ssl));
+		ESP_LOGI(TAG, "Cipher suite is %s", mbedtls_ssl_get_ciphersuite(&ssl));
 
-        ESP_LOGI(TAG, "Writing HTTP request...");
+		ESP_LOGI(TAG, "Writing HTTP request...");
 
-        size_t written_bytes = 0;
-        do {
-            ret = mbedtls_ssl_write(&ssl,
-                                    (const unsigned char *)REQUEST + written_bytes,
-                                    strlen(REQUEST) - written_bytes);
-            if (ret >= 0) {
-                ESP_LOGI(TAG, "%d bytes written", ret);
-                written_bytes += ret;
-            } else if (ret != MBEDTLS_ERR_SSL_WANT_WRITE && ret != MBEDTLS_ERR_SSL_WANT_READ) {
-                ESP_LOGE(TAG, "mbedtls_ssl_write returned -0x%x", -ret);
-                goto exit;
-            }
-        } while(written_bytes < strlen(REQUEST));
+		size_t written_bytes = 0;
+		do {
+			ret = mbedtls_ssl_write(&ssl,
+									(const unsigned char *)REQUEST + written_bytes,
+									strlen(REQUEST) - written_bytes);
+			if (ret >= 0) {
+				ESP_LOGI(TAG, "%d bytes written", ret);
+				written_bytes += ret;
+			} else if (ret != MBEDTLS_ERR_SSL_WANT_WRITE && ret != MBEDTLS_ERR_SSL_WANT_READ) {
+				ESP_LOGE(TAG, "mbedtls_ssl_write returned -0x%x", -ret);
+				goto exit;
+			}
+		} while(written_bytes < strlen(REQUEST));
 
-        ESP_LOGI(TAG, "Reading HTTP response...");
+		ESP_LOGI(TAG, "Reading HTTP response...");
 
-        do
-        {
-            len = sizeof(buf) - 1;
-            bzero(buf, sizeof(buf));
-            ret = mbedtls_ssl_read(&ssl, (unsigned char *)buf, len);
+		do
+		{
+			len = sizeof(buf) - 1;
+			bzero(buf, sizeof(buf));
+			ret = mbedtls_ssl_read(&ssl, (unsigned char *)buf, len);
 
-            if(ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE)
-                continue;
+			if(ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE)
+				continue;
 
-            if(ret == MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY) {
-                ret = 0;
-                break;
-            }
+			if(ret == MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY) {
+				ret = 0;
+				break;
+			}
 
-            if(ret < 0)
-            {
-                if (ret == -0x6800)
-                {
-                	printf("Expected timeout, not an error.\n");
-                	ret = 0;
-                }
-                else
-                {
-               		ESP_LOGE(TAG, "mbedtls_ssl_read returned -0x%x", -ret);
-                }
+			if(ret < 0)
+			{
+				if (ret == -0x6800)
+				{
+					printf("Expected timeout, not an error.\n");
+					ret = 0;
+				}
+				else
+				{
+					ESP_LOGE(TAG, "mbedtls_ssl_read returned -0x%x", -ret);
+				}
 
-                break;
-            }
+				break;
+			}
 
-            if(ret == 0)
-            {
-                ESP_LOGI(TAG, "connection closed");
-                break;
-            }
+			if(ret == 0)
+			{
+				ESP_LOGI(TAG, "connection closed");
+				break;
+			}
 
-            len = ret;
-            ESP_LOGD(TAG, "%d bytes read", len);
-            /* Print response directly to stdout as it is read */
-            for(int i = 0; i < len; i++) {
-                putchar(buf[i]);
-            }
-        } while(1);
+			len = ret;
+			ESP_LOGD(TAG, "%d bytes read", len);
+			/* Print response directly to stdout as it is read */
+			for(int i = 0; i < len; i++) {
+				putchar(buf[i]);
+			}
+		} while(1);
 
-        mbedtls_ssl_close_notify(&ssl);
+		mbedtls_ssl_close_notify(&ssl);
 
-    exit:
-        mbedtls_ssl_session_reset(&ssl);
-        mbedtls_net_free(&server_fd);
+	exit:
+		mbedtls_ssl_session_reset(&ssl);
+		mbedtls_net_free(&server_fd);
 
-        if(ret != 0)
-        {
-            mbedtls_strerror(ret, buf, 100);
-            ESP_LOGE(TAG, "Last error was: -0x%x - %s", -ret, buf);
-        }
+		if(ret != 0)
+		{
+			mbedtls_strerror(ret, buf, 100);
+			ESP_LOGE(TAG, "Last error was: -0x%x - %s", -ret, buf);
+		}
 
-        putchar('\n'); // JSON output doesn't have a newline at end
+		putchar('\n'); // JSON output doesn't have a newline at end
 
-        static int request_count;
-        ESP_LOGI(TAG, "Completed %d requests", ++request_count);
+		static int request_count;
+		ESP_LOGI(TAG, "Completed %d requests", ++request_count);
 
-        for(int countdown = 10; countdown >= 0; countdown--) {
-            ESP_LOGI(TAG, "%d...", countdown);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        ESP_LOGI(TAG, "Starting again!");
-    }
+		for(int countdown = 10; countdown >= 0; countdown--) {
+			ESP_LOGI(TAG, "%d...", countdown);
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+		}
+		ESP_LOGI(TAG, "Starting again!");
+	}
 }
 
 
