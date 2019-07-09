05c030f86dd34208ddae6bb9277243a5dba8807e
Matias Brignone <mnbrignone@gmail.com>
2019-07-09 19:58:24 -0300

    create separate directory for http/s tasks

--- main/component.mk
+++ main/component.mk
@@ -1,5 +1,7 @@
 
-COMPONENT_ADD_INCLUDEDIRS += ./inc
+
+COMPONENT_SRCDIRS += ./tasks
+COMPONENT_ADD_INCLUDEDIRS += ./inc ./tasks/inc
 
 COMPONENT_EMBED_TXTFILES := certificate/thingspeak_https_certificate.pem
 COMPONENT_EMBED_TXTFILES += certificate/thingspeak_mqtts_certificate.pem
