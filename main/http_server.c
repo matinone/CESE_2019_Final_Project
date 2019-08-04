de0a92dbcad36341635b1a4777dd0166a6bca67e
Matias Brignone <mnbrignone@gmail.com>
2019-08-04 17:57:01 -0300

    improve web page

--- main/http_server.c
+++ main/http_server.c
@@ -16,34 +16,15 @@
 #include "lwip/netdb.h"
 
 // HTTP headers and web pages
-const static char http_html_hdr[] = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
-const static char http_png_hdr[] = "HTTP/1.1 200 OK\nContent-type: image/png\n\n";
-const static char http_wifi_hml[] = "<HTML>"
-    "<HEAD>"
-        "<TITLE>ESP32 Web Server</TITLE>"
-"<meta content=\"width=device-width,initial-scale=1\"name=viewport>"
-"<style>div{width:230px;height:300px;position:absolute;top:0;bottom:0;left:0;right:0;margin:auto}</style>"
-"<link rel=\"icon\" href=\"data:;base64,=\">"
-    "</HEAD>"
-"<BODY>"
-    "<CENTER>"
-"<div>"
-"<h1 align=center>ESP32 WiFi Credentials</h1><a href=off.html><img src=on.png></a>"
-    "<form action=\"/form_page\" method=\"post\">"
-        "SSID: <input type=\"text\" name=\"ssid\"><br>"
-        "Password: <input type=\"text\" name=\"password\"><br>"
-        "<input type=\"submit\" value=\"Connect to WiFi\">"
-    "</form>"
-"</div>"
-    "</CENTER>" 
-"</BODY>"
-"</HTML>";
+const static char http_html_header[] = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
+const static char http_png_header[] = "HTTP/1.1 200 OK\nContent-type: image/png\n\n";
 
 // embedded binary data
-extern const uint8_t on_png_start[] asm("_binary_on_png_start");
-extern const uint8_t on_png_end[]   asm("_binary_on_png_end");
-// extern const uint8_t off_png_start[] asm("_binary_off_png_start");
-// extern const uint8_t off_png_end[]   asm("_binary_off_png_end");
+extern const uint8_t wifi_icon_png_start[] asm("_binary_wifi_icon_png_start");
+extern const uint8_t wifi_icon_png_end[]   asm("_binary_wifi_icon_png_end");
+
+extern const uint8_t index_html_start[] asm("_binary_index_html_start");
+extern const uint8_t index_html_end[]   asm("_binary_index_html_end");
 
 static void http_server_netconn_serve(struct netconn *conn);
 static char* recover_encoded_spaces(char* encoded_string);
@@ -94,23 +75,23 @@ static void http_server_netconn_serve(struct netconn *conn)
                 // default page
                 if(strcmp(req->resource, "/") == 0) 
                 {
-                    netconn_write(conn, http_html_hdr, sizeof(http_html_hdr) - 1, NETCONN_NOCOPY);
+                    netconn_write(conn, http_html_header, sizeof(http_html_header) - 1, NETCONN_NOCOPY);
                     // printf("Sending default page, relay is OFF\n");
-                    netconn_write(conn, http_wifi_hml, sizeof(http_wifi_hml) - 1, NETCONN_NOCOPY);
+                    netconn_write(conn, index_html_start, index_html_end - index_html_start, NETCONN_NOCOPY);
                 }         
                 // OFF page
-                else if(strcmp(req->resource, "/off.html") == 0) 
+                else if(strcmp(req->resource, "/index.html") == 0) 
                 {
                     // printf("Sending OFF page...\n");
-                    netconn_write(conn, http_html_hdr, sizeof(http_html_hdr) - 1, NETCONN_NOCOPY);
-                    netconn_write(conn, http_wifi_hml, sizeof(http_wifi_hml) - 1, NETCONN_NOCOPY);
+                    netconn_write(conn, http_html_header, sizeof(http_html_header) - 1, NETCONN_NOCOPY);
+                    netconn_write(conn, index_html_start, index_html_end - index_html_start, NETCONN_NOCOPY);
                 }
                 // ON image
-                else if(strcmp(req->resource, "/on.png") == 0) 
+                else if(strcmp(req->resource, "/wifi_icon.png") == 0) 
                 {
-                    // printf("Sending ON image...\n");
-                    netconn_write(conn, http_png_hdr, sizeof(http_png_hdr) - 1, NETCONN_NOCOPY);
-                    netconn_write(conn, on_png_start, on_png_end - on_png_start, NETCONN_NOCOPY);
+                    // printf("Sending WiFi icon image...\n");
+                    netconn_write(conn, http_png_header, sizeof(http_png_header) - 1, NETCONN_NOCOPY);
+                    netconn_write(conn, wifi_icon_png_start, wifi_icon_png_end - wifi_icon_png_start, NETCONN_NOCOPY);
                 }
                 else
                 {
@@ -125,8 +106,8 @@ static void http_server_netconn_serve(struct netconn *conn)
                     printf("Received form with body: \n");
                     recover_encoded_spaces(req->body);
                     puts(req->body);
-                    netconn_write(conn, http_html_hdr, sizeof(http_html_hdr) - 1, NETCONN_NOCOPY);
-                    netconn_write(conn, http_wifi_hml, sizeof(http_wifi_hml) - 1, NETCONN_NOCOPY);
+                    netconn_write(conn, http_html_header, sizeof(http_html_header) - 1, NETCONN_NOCOPY);
+                    netconn_write(conn, index_html_start, index_html_end - index_html_start, NETCONN_NOCOPY);
 
                     char * pch = strtok (req->body,"=&");   // pch here is "ssid"
                     pch = strtok(NULL, "=&");               // pch here is the ssid value
